4626020701b7ea82c36aba209c93b84403a3f4b4
Mauricio Junior <mjrfmoraes@outlook.com>
2022-11-02 17:52:12 -0400

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -41,5 +41,32 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+           if(id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var obj = _sellerService.FindById(id.Value);
+                if(obj == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(obj);
+                }
+            }
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
